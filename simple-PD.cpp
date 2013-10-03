53197ff275c79b933d6a8240be5b1e62193c4e98
Evan Drumwright <drum@pauling.seas.gwu.edu>
2013-10-03 10:09:02 -0400

    Updated HyQ model and added simple PD controller

---
+++ simple-PD.cpp
@@ -0,0 +1,300 @@
+/*****************************************************************************
+ * Simple PD controller 
+ ****************************************************************************/
+
+#include <cmath>
+#include <sys/types.h>
+#include <sys/times.h>
+#include <fstream>
+
+#include <boost/shared_ptr.hpp>
+#include <Ravelin/MatrixNd.h>
+#include <Ravelin/VectorNd.h>
+#include <Ravelin/Vector3d.h>
+#include <Ravelin/LinAlgd.h>
+#include <Ravelin/AAngled.h>
+#include <Ravelin/SForced.h>
+
+
+#include <Moby/Simulator.h>
+#include <Moby/EventDrivenSimulator.h>
+#include <Moby/RCArticulatedBody.h>
+#include <Moby/DeformableBody.h>
+#include <Moby/DynamicBody.h>
+#include <Moby/RevoluteJoint.h>
+#include <Moby/GravityForce.h>
+#include <Moby/Constants.h>
+#include <Moby/RNEAlgorithm.h>
+
+using namespace Moby;
+using namespace Ravelin;
+using boost::shared_ptr;
+using boost::dynamic_pointer_cast;
+using std::string;
+using std::map;
+using std::pair;
+using std::vector;
+
+enum RobotDOFs {
+  LF_HFE = 0,
+  LF_HAA,
+  LF_KFE,
+
+  LH_HFE,
+  LH_HAA,
+  LH_KFE,
+
+  RF_HFE,
+  RF_HAA,
+  RF_KFE,
+
+  RH_HFE,
+  RH_HAA,
+  RH_KFE,
+
+  NJOINT
+};
+
+const unsigned NUM_EEFS = 4,
+               NDOFS = NJOINT+6,
+               N_FIXED_JOINTS = 4,
+	       NSPATIAL = 6;
+
+typedef Ravelin::MatrixNd Mat;
+typedef Ravelin::VectorNd Vec;
+
+std::vector<std::string> eef_names_, joint_name_;
+std::vector<RigidBodyPtr> eefs;
+static map<string, double> q_go0;
+// simulator 
+boost::shared_ptr<EventDrivenSimulator> sim;
+// robot pointer
+RCArticulatedBodyPtr abrobot;
+DynamicBodyPtr dbrobot;
+
+// integration error
+map<string, double> perr_sum;
+
+struct Gains
+{
+  double kp;
+  double kv;
+  double ki;
+};
+
+// map of gains
+map<string, Gains> gains;
+
+template <typename T, typename U>
+const U& get(const map<T, U>& m, const T& key)
+{
+  typename map<T, U>::const_iterator i = m.find(key);
+  if (i == m.end())
+    throw std::runtime_error("Unexpectedly couldn't find key in map!");
+  return i->second;
+}
+
+    double last_time = 0;
+/// Controls the robot
+void control_PID(const map<string, double>& q_des, const map<string, double>& qd_des, const map<string, Gains>& gains, double time,Mat& ufb)
+{
+      const std::vector<JointPtr>& joints = abrobot->get_joints();
+
+      // clear and set motor torques
+      for (unsigned m=0,i=0; m< joints.size(); m++)
+      {
+            // get the joint
+            JointPtr j = joints[m];
+            if(j->q.rows() == 0) continue;
+
+            // get the two gains
+            const double KP = get(gains,j->id).kp;
+            const double KV = get(gains,j->id).kv;
+            const double KI = get(gains,j->id).ki;
+
+            // add feedback torque to joints
+            double perr = get(q_des,j->id) - j->q[0];
+            perr_sum[j->id] += perr;
+            double ierr = perr_sum[j->id];
+            double derr = get(qd_des,j->id) - j->qd[0];
+            Vec fb_torque(1);
+            fb_torque[0] = perr*KP + derr*KV + ierr*KI;
+            ufb.set_row(i,fb_torque);
+            i++;
+      }
+}
+
+
+
+void apply_simulation_forces(const Mat& u){
+    const std::vector<JointPtr>& joints = abrobot->get_joints();
+    for(unsigned m=0,i=0;m< joints.size();m++){
+        if(joints[m]->q.size() == 0) continue;
+        // reset motor torque
+        Vec row;
+        joints[m]->reset_force();
+        joints[m]->add_force(u.get_row(i,row));
+        i++;
+    }
+}
+
+/// The main control loop
+void controller(DynamicBodyPtr dbp, double t, void*)
+{
+    static Mat ufb(NJOINT,1),u(NJOINT,1), q(NJOINT,1), qd(NJOINT,1);
+    double dt = t - last_time;
+     
+     static unsigned ITER = 1;
+
+     ufb.set_zero();
+     u.set_zero();
+
+      /// setup a steady state
+      static map<string, double> q_des, qd_des;
+      const std::vector<JointPtr>& joints = abrobot->get_joints();
+      if (q_des.empty())
+      {
+        std::cout << "desired q/qd: ";
+        for (unsigned i=0,m=0; m< joints.size(); m++)
+        {
+            if(joints[m]->q.size() == 0) continue;
+            q_des[joints[m]->id] = q_go0[joints[m]->id];
+            qd_des[joints[m]->id] = 0.0;
+            std::cout << "(" << q_des[joints[m]->id] << ",0) ";
+            i++;
+        }
+      }
+      std::cout << std::endl;
+
+      std::cout << "current q/qd: ";
+      ///  Record Robot State
+      for(unsigned m=0;m< joints.size();m++){
+          if(joints[m]->q.size() == 0) continue;
+          unsigned ind = joints[m]->get_coord_index();
+          q.set_row(ind,joints[m]->q);
+          qd.set_row(ind,joints[m]->qd);
+          std::cout << "(" << joints[m]->q[0] << "," << joints[m]->qd[0] << ") ";
+      }
+      std::cout << std::endl;
+
+      ///  Determine FB forces
+      control_PID(q_des, qd_des, gains,t,ufb);
+
+      // combine ufb and uff
+      u += ufb; 
+
+      apply_simulation_forces(u);
+
+      if(dt>0){
+        std::cerr << "iteration: " << ITER << std::endl;
+      	ITER++;
+      }
+
+
+    last_time = t;
+}
+
+
+/// plugin must be "extern C"
+extern "C" {
+
+void init(void* separator, const std::map<std::string, BasePtr>& read_map, double time)
+{
+  // get a reference to the EventDrivenSimulator instance
+  for (std::map<std::string, BasePtr>::const_iterator i = read_map.begin(); i !=read_map.end(); i++) 
+  {
+    if (!sim)
+      sim = dynamic_pointer_cast<EventDrivenSimulator>(i->second);
+
+    // find the robot
+    if (!abrobot)
+    {
+      abrobot = dynamic_pointer_cast<RCArticulatedBody>(i->second);
+      dbrobot = dynamic_pointer_cast<DynamicBody>(i->second);
+    }
+  }
+  // setup the controller
+  abrobot->controller = &controller;
+  // Create joint name vector
+  joint_name_.push_back("LF_HFE");
+  joint_name_.push_back("LF_HAA");
+  joint_name_.push_back("LF_KFE");
+  joint_name_.push_back("LH_HFE");
+  joint_name_.push_back("LH_HAA");
+  joint_name_.push_back("LH_KFE");
+  joint_name_.push_back("RF_HFE");
+  joint_name_.push_back("RF_HAA");
+  joint_name_.push_back("RF_KFE");
+  joint_name_.push_back("RH_HFE");
+  joint_name_.push_back("RH_HAA");
+  joint_name_.push_back("RH_KFE");
+  
+  // robot's go0 configuration
+  q_go0["LF_HFE"] =  0.6;
+  q_go0["LF_HAA"] = -0.35;
+  q_go0["LF_KFE"] = -1.4;
+
+  q_go0["RF_HFE"] =  0.6;
+  q_go0["RF_HAA"] = -0.35;
+  q_go0["RF_KFE"] = -1.4;
+
+  q_go0["LH_HFE"] = -0.6;
+  q_go0["LH_HAA"] = -0.35;
+  q_go0["LH_KFE"] =  1.4;
+
+  q_go0["RH_HFE"] = -0.6;
+  q_go0["RH_HAA"] = -0.35;
+  q_go0["RH_KFE"] =  1.4;
+
+  eef_names_.push_back("LF_FOOT");
+  eef_names_.push_back("RF_FOOT");
+  eef_names_.push_back("LH_FOOT");
+  eef_names_.push_back("RH_FOOT");
+
+  // now, setup gains
+  for(unsigned i=0;i<joint_name_.size();i++){
+      double kp,kv,ki;
+      switch(i%3){
+      case 0:
+          kp = 300;
+          kv = 6;
+          ki = 0;
+          break;
+      case 1:
+          kp = 10;
+          kv = 6;
+          ki = 0;
+          break;
+      case 2:
+          kp = 300;
+          kv = 6;
+          ki = 0;
+          break;
+      default: break;
+      }
+      // pass gain values to respective joint
+      gains[joint_name_[i]].kp = kp;
+      gains[joint_name_[i]].kv = kv;
+      gains[joint_name_[i]].ki = ki;
+  }
+
+      Vec q_start(q_go0.size()+7),qd_start(q_go0.size()+6);
+      q_start.set_zero();
+      qd_start.set_zero();
+
+      for(int i=0;i<q_go0.size();i++){
+          int ind = abrobot->find_joint(joint_name_[i])->get_coord_index();
+          q_start[ind+1] = q_go0[joint_name_[i]];
+          std::cout << ind << " " << joint_name_[i] << std::endl;
+      }
+
+      q_start[2] = 1.05;
+      for(int i=0;i<q_start.rows();i++)
+          std::cerr << q_start[i] << std::endl;
+
+      abrobot->set_generalized_coordinates(DynamicBody::eEuler,q_start);
+      abrobot->set_generalized_velocity(DynamicBody::eSpatial,qd_start);
+}
+
+} // end extern C
+

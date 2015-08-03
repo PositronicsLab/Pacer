fc36a05abb3427486bdcf5a497baa320eaa615c0
Samuel Zapolsky <samzapo@Roxanas-MacBook-Pro.local>
2015-08-03 17:11:42 -0400

    removed logging files

--- example/plugins/stand-trajectory.cpp
+++ example/plugins/stand-trajectory.cpp
@@ -18,18 +18,16 @@ void Update(const boost::shared_ptr<Pacer::Controller>& ctrl, double t){
   ctrl->set_joint_generalized_value(Pacer::Controller::velocity_goal,qd_goal);
   ctrl->set_joint_generalized_value(Pacer::Controller::acceleration_goal,qdd_goal);
   
-  
-  ctrl->set_model_state(q_goal);
-  boost::shared_ptr<Ravelin::Pose3d> base_frame( new Ravelin::Pose3d(
-        ctrl->get_data<Ravelin::Pose3d>("base_link_frame")));
   static std::vector<std::string>
       foot_names = ctrl->get_data<std::vector<std::string> >("init.end-effector.id");
 
+  boost::shared_ptr<Ravelin::Pose3d> base_frame = boost::shared_ptr<Ravelin::Pose3d>( new Ravelin::Pose3d(
+        ctrl->get_data<Ravelin::Pose3d>("base_link_frame")));
   for(int i=0;i<foot_names.size();i++){
-    Ravelin::Vector3d x 
-      = Ravelin::Pose3d::transform_point(base_frame,Ravelin::Vector3d(0,0,0,ctrl->get_link(foot_names[i])->get_pose())),
+    Ravelin::Vector3d x = ctrl->get_data<Ravelin::Vector3d>(foot_names[i]+".init.x"),
       xd(0,0,0,base_frame), 
       xdd(0,0,0,base_frame);
+    x.pose = base_frame;
     ctrl->set_data<Ravelin::Vector3d>(foot_names[i]+".goal.x",x);
     ctrl->set_data<Ravelin::Vector3d>(foot_names[i]+".goal.xd",xd);
     ctrl->set_data<Ravelin::Vector3d>(foot_names[i]+".goal.xdd",xdd);
--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 export PACER_HOME=$PWD
 export PACER_MODEL_PATH=$PACER_HOME/example/models
-export PACER_PLUGIN_PATH=$PACER_HOME/build/example/plugins
+export PACER_PLUGIN_PATH=$PACER_HOME/debug/example/plugins
 export GAZEBO_MODEL_PATH=$PACER_HOME/example/models:$GAZEBO_MODEL_PATH
-export GAZEBO_PLUGIN_PATH=$PACER_HOME/build/example/interfaces:$GAZEBO_PLUGIN_PATH
+export GAZEBO_PLUGIN_PATH=$PACER_HOME/debug/example/interfaces:$GAZEBO_PLUGIN_PATH

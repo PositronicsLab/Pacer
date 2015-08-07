0fd854a4ffc786a7f0196a06cbf891cdab047f2f
samzapo <samzapo@gmail.com>
2015-08-07 11:58:11 -0400

    added stepped heightmap

--- test/Quadruped/waypoints-terrain/heightmap.py
+++ test/Quadruped/waypoints-terrain/heightmap.py
@@ -2,22 +2,32 @@ from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn import mixture
+import time
 
 def fit_samples(samples, ncomponents):
   gmix = mixture.GMM(n_components=ncomponents, covariance_type='full')
   gmix.fit(samples)
   return gmix
 
-np.random.seed(0)
-samples = np.random.rand(10000, 2)
+print time.time()
+np.random.seed(int(time.time()))
+samples = np.random.rand(1000, 2)
 gmix = fit_samples(samples,100)
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-x = y = np.arange(-3.0, 3.0, 0.05)
+x = y = np.arange(0, 1.0, 0.01)
 X, Y = np.meshgrid(x, y)
-zs = np.array([gmix.predict([x,y]) for x,y in zip(np.ravel(X), np.ravel(Y))])
-Z = zs.reshape(X.shape)
+z = gmix.predict(zip(np.ravel(X), np.ravel(Y)))
+z = z * 0.05/max(z)
+z = z - min(z)
+
+Z = z.reshape(X.shape)
+
+zsize = Z.shape
+zsize_txt = ''.join((str(zsize[0]),' ',str(zsize[1])))
+print zsize_txt
+np.savetxt('heightmap.dat',Z,delimiter=' ',header=zsize_txt)
 
 ax.plot_surface(X, Y, Z)
 
@@ -26,3 +36,4 @@ ax.set_ylabel('Y Label')
 ax.set_zlabel('Z Label')
 
 plt.show()
+

74a8d54d856789158e4fb7746a6f9f37a5c49ee6
Samuel Zapolsky <samzapo@gmail.com>
2015-12-07 14:43:47 -0500

    jumping test fails due to variable removal, fix on its way

--- build-and-test.sh
+++ build-and-test.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 # Local Build
 mkdir $1; cd $1;
-cmake -DBUILD_EXAMPLES=ON -DBUILD_INTERFACES=ON -DBUILD_PLUGINS=ON -DBUILD_REGRESSION_TESTS=ON -DBUILD_UNIT_TESTS=OFF -DBUILD_TESTS=ON -DUSE_OSG_DISPLAY=ON -DCMAKE_BUILD_TYPE=$1 ..;
-make -j 4;
+cmake -DBUILD_EXAMPLES=ON -DBUILD_INTERFACES=ON -DBUILD_PLUGINS=ON -DBUILD_REGRESSION_TESTS=ON -DBUILD_UNIT_TESTS=OFF -DBUILD_TESTS=ON -DUSE_OSG_DISPLAY=ON -DCMAKE_BUILD_TYPE=$1 -DUSE_GTEST=ON ..;
+make -j 18;
 # Regression Test
-export GTEST_OUTPUT="xml:./$1/";
-source $1/setup.sh;
+export GTEST_OUTPUT="xml:./";
+source setup.sh;
 #convert @@EXPORTED_VALUE@@ tokens to value: $(echo ${EXPORTED_VALUE})
-./test/RegressionTests/setup-tests.sh ./test/RegressionTests/*/*.in
+${DIR}/test/RegressionTests/setup-tests.sh ${DIR}/test/RegressionTests/*/*.in
 ( make regression-test ; exit 0);

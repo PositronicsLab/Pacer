33e064ed32a99860df7b12625a133d71e897f731
samzapo <samzapo@gmail.com>
2016-02-02 15:10:22 -0500

    fixed test scripts

--- Example/Script/run-test.sh
+++ Example/Script/run-test.sh
@@ -3,6 +3,6 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 # Local Build
 # Regression Test
 #convert @@EXPORTED_VALUE@@ tokens to value: $(echo ${EXPORTED_VALUE})
-git clean -dfx ${DIR}/test/
-${DIR}/test/RegressionTests/setup-tests.sh ${DIR}/test/RegressionTests/*/*.in
+git clean -dfx ${PACER_HOME}
+${PACER_SCRIPT_PATH}/setup-tests.sh ${PACER_HOME}/Example/Demo/*/*.in
 ( make regression-test ; exit 0);
